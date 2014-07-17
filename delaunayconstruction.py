e86981d1c004f298d57c5aa132490b3ca5a7cc51
Pio Calderon <piocalderon@gmail.com>
2014-07-18 03:17:54 +0800

    Fix region-on-edge issue. Issue with 2 sample voronoi. Another issue on not being able to voronoi complicated patterns and detect thin boundaries.

--- delaunayconstruction.py
+++ delaunayconstruction.py
@@ -8,13 +8,15 @@ Created on Sat Jan 04 15:00:10 2014
 """
 
 from scipy.spatial import Delaunay
-from scipy.spatial import Voronoi
+from scipy.spatial import Voronoi, voronoi_plot_2d
 import numpy as np
 import networkx as nx
 import extractregions
 import matplotlib.pyplot as plt
 from shapely.geometry import Polygon, Point
 from descartes.patch import PolygonPatch
+
+#countez = 0
 
 def return_sample_list(num, regions, dist, scale):
     """
@@ -42,30 +44,6 @@ def return_sample_list(num, regions, dist, scale):
  
     print "-done sampling-"
     return sample_list    
-
-#def return_sample_list(total, shape, dist, scale):
-#    """
-#    Return a list containing the position (x,y) of the samples.
-#    """
-#    
-#    if shape == "circle":
-#        p = Point(0.5,0.5)
-#        shape = p.buffer(.5)
-#    elif shape == "square":
-#        shape = Polygon([(0., 0.), (0., 1.), (1., 1.), (1., 0.)])
-#        
-#    sampleList = []
-#    while len(sampleList) != total:
-#        if dist == "normal":            
-#            candidate = (np.random.normal(loc = 0.5, scale = scale), \
-#            np.random.normal(loc = 0.5, scale = scale))            
-#        if dist == "uniform":           
-#            candidate = (np.random.uniform(),np.random.uniform())                        
-#        if shape.contains(Point(candidate)):            
-#            sampleList.append(candidate)
-# 
-#    print "-done sampling-"
-#    return sampleList
 
 def return_land_use_of_region(region_samples, numType, mode=0):
     """
@@ -139,89 +117,9 @@ def return_land_use_of_region(region_samples, numType, mode=0):
     
 def return_land_use(sample_list, mode):
     land_use = []
-    for region_samples in sample_list:
+    for region_samples in sample_list.values():
         land_use.append(return_land_use_of_region(region_samples, mode))   
     return land_use
-
-#def return_land_use(sampleList, numType, occupationProbability):
-#    """
-#    Return the landuse type of the samples in the format (type1,type2,type3).
-#    """
-#    
-#    totalUse = {}
-#    for index1, sample in enumerate(sampleList):       
-#        if np.random.uniform() > occupationProbability:
-#      #      totalUse.append([0,0,0,1]) # unoccupied
-#            continue               
-# 
-#        sampleUse = []             
-#        radius = np.sqrt((sample[0]-0.5)**2 + (sample[1]-0.5)**2)
-#
-#        for index in range(numType):               
-#        #    if index == numType - 1:
-#        #        sampleUse.append(0)
-#        #        continue            
-#            sampleUse.append(np.random.uniform()) # RUN 01                    
-#            
-##            if index == 0:
-##                sampleUse.append(5*np.random.uniform()) # RUN 02
-##            else:
-##                sampleUse.append(np.random.uniform())
-##            """
-##            """
-##            if index == 0 or index == 1:
-##                sampleUse.append(5*np.random.uniform()) # RUN 03
-##            else:
-##                sampleUse.append(np.random.uniform())
-##            """
-##            """
-##            if index == 0:
-##                if np.sqrt((sample[0]-0.5)**2 + (sample[1]-0.5)**2) < 0.2: # RUN 04
-##                    sampleUse.append(5*np.random.uniform())
-##                else:
-##                    sampleUse.append(0)
-##            else:
-##                sampleUse.append(np.random.uniform())
-##            """
-##            """
-##            if index == 0:
-##                if radius < 0.167: # RUN 05
-##                    sampleUse.append(10*np.random.uniform())
-##                else:
-##                    sampleUse.append(np.random.uniform())
-##            if index == 2:
-##                if 0.167 <= radius < 0.334:
-##                    sampleUse.append(10*np.random.uniform())
-##                else:
-##                    sampleUse.append(np.random.uniform())
-##            if index == 1:
-##                if 0.334 <= radius:
-##                    sampleUse.append(10*np.random.uniform())
-##                else:
-##                    sampleUse.append(np.random.uniform())
-##            """
-##            """
-##            if index == 0:
-##                if sample[0] < 0.333: # RUN 06
-##                    sampleUse.append(10*np.random.uniform())
-##                else:
-##                    sampleUse.append(np.random.uniform())
-##            if index == 1:
-##                if 0.333 <= sample[0] < 0.666:
-##                    sampleUse.append(10*np.random.uniform())
-##                else:
-##                    sampleUse.append(np.random.uniform())
-##            if index == 2:
-##                if 0.666 <= sample[0]:
-##                    sampleUse.append(10*np.random.uniform())
-##                else:
-##                    sampleUse.append(np.random.uniform())
-##            
-#        total = sum(sampleUse)
-#        sampleUse = [1.0*sampleUse[i]/total for i in xrange(len(sampleUse))]
-#        totalUse[index1] = sampleUse
-#        
-#    return totalUse
 
 # http://stackoverflow.com/questions/12374781/how-to-find-all-neighbors-of-a-given-point-in-a-delaunay-triangulation-using-sci
 def find_neighbors(pindex, triangulation):
@@ -245,14 +143,27 @@ def construct_delaunay_network_of_region(region, region_samples, region_land_use
     """
     Construct the delaunay network of a region from region samples and land use.
     """
+
     region_boundary = Polygon(region)
+
+#    fig = plt.figure(figsize = (10, 10), facecolor = "black")
+#    ax = fig.add_subplot(111)#fig.add_axes([0, 0, 1, 1])
+#    ax.set_xlim(0,dimensions[0])
+#    ax.set_ylim(0,dimensions[1])    
+#    patch = PolygonPatch(region_boundary, fc = "red", ec = "green",\
+#                aa=False)
+#    ax.add_patch(patch)
+#    plt.plot()
+
+    
     for interior in interior_polygons:
         if interior.within(region_boundary):
             region_boundary = region_boundary.difference(interior)
+    
     totalArea = region_boundary.area
     G = nx.Graph()
     region_samples = np.asarray(region_samples)
-    
+
     multiCount = 0    
     multiLookUp = []    
     
@@ -260,13 +171,12 @@ def construct_delaunay_network_of_region(region, region_samples, region_land_use
     vor = Voronoi(region_samples) 
     # return neighborlist for each node in network
     neighborList = return_neighbor_list(region_samples) 
-
 # FROM https://gist.github.com/pv/8036995#file-colorized_voronoi-py-L48 
     
     new_regions = []
     new_vertices = vor.vertices.tolist()
     center = vor.points.mean(axis=0)
-    radius = 100  
+    radius = 1000  
 
     # Construct a map containing all ridges for a given point
     all_ridges = {}
@@ -312,7 +222,7 @@ def construct_delaunay_network_of_region(region, region_samples, region_land_use
  
         # finish
         new_regions.append([p1, new_region.tolist()])
-    
+
     # iterate over each voronoi region
     for sampleIndex, simplex in new_regions:     
         if sampleIndex in region_land_use.keys():
@@ -326,10 +236,10 @@ def construct_delaunay_network_of_region(region, region_samples, region_land_use
                         [component][1]))
 
                 if len(pointList) > 2:
-
+      
                     polygon = Polygon(pointList)
                     polygon = region_boundary.intersection(polygon)
-                    
+                                    
                     if polygon.geom_type == 'MultiPolygon':
                         for index, p in enumerate(polygon):
                             area = p.area
@@ -361,7 +271,7 @@ def construct_delaunay_network_of_region(region, region_samples, region_land_use
             if focusPolygon.touches(neighborPolygon):
                 neighbors.append(nindex)
         neighborList.append(neighbors)
-        
+
     for index in G.nodes():
         
         for nindex in range(len(list(neighborList[index]))):
@@ -373,147 +283,31 @@ def construct_delaunay_network_of_region(region, region_samples, region_land_use
 
     print "-done constructing network-"
     return G
-    
-#def construct_delaunay_network(sampleList, landUse, shape):
-#    """
-#    Construct the delaunay network from the samplelist and landuse.
-#    """
-#
-#    if shape == "circle":
-#
-#        p = Point(0.5,0.5)
-#        shape = p.buffer(.5)
-#
-#    elif shape == "square":
-#
-#        shape = Polygon([(0., 0.), (0., 1.), (1., 1.), (1., 0.)])   
-#        
-#    totalArea = shape.area
-#    G = nx.Graph()
-#    sampleList = np.asarray(sampleList)
-#    # construct voronoi object
-#    vor = Voronoi(sampleList) 
-#    # return neighborlist for each node in network
-#    neighborList = return_neighbor_list(sampleList) 
-#
-## FROM https://gist.github.com/pv/8036995#file-colorized_voronoi-py-L48 
-#    
-#    new_regions = []
-#    new_vertices = vor.vertices.tolist()
-#    center = vor.points.mean(axis=0)
-#    radius = 100  
-#
-#    # Construct a map containing all ridges for a given point
-#    all_ridges = {}
-#
-#    for (p1, p2), (v1, v2) in zip(vor.ridge_points, vor.ridge_vertices):
-#
-#        all_ridges.setdefault(p1, []).append((p2, v1, v2))
-#        all_ridges.setdefault(p2, []).append((p1, v1, v2))    
-#
-#    for p1, region in enumerate(vor.point_region):
-#
-#        vertices = vor.regions[region]
-#
-#        if all([v >= 0 for v in vertices]):
-#
-#            # finite region
-#            new_regions.append([p1,vertices])
-#            continue
-#
-#        # reconstruct a non-finite region
-#        ridges = all_ridges[p1]
-#        new_region = [v for v in vertices if v >= 0]
-# 
-#        for p2, v1, v2 in ridges:
-#
-#            if v2 < 0:
-#
-#                v1, v2 = v2, v1
-#
-#            if v1 >= 0:
-#
-#                # finite ridge: already in the region
-#                continue
-# 
-#            # Compute the missing endpoint of an infinite ridge
-#            t = vor.points[p2] - vor.points[p1] # tangent
-#            t /= np.linalg.norm(t)
-#            n = np.array([-t[1], t[0]])  # normal
-# 
-#            midpoint = vor.points[[p1, p2]].mean(axis=0)
-#            direction = np.sign(np.dot(midpoint - center, n)) * n
-#            far_point = vor.vertices[v2] + direction * radius
-#            new_region.append(len(new_vertices))
-#            new_vertices.append(far_point.tolist())
-# 
-#        # sort region counterclockwise
-#        vs = np.asarray([new_vertices[v] for v in new_region])
-#        c = vs.mean(axis=0)
-#        angles = np.arctan2(vs[:,1] - c[1], vs[:,0] - c[0])
-#        new_region = np.array(new_region)[np.argsort(angles)]
-# 
-#        # finish
-#        new_regions.append([p1, new_region.tolist()])
-#    
-#    # iterate over each voronoi region
-#    for sampleIndex, simplex in new_regions: 
-#    
-#        if sampleIndex in landUse.keys():
-#
-#            simplex = np.asarray(simplex)
-#            
-#            # check if region contains vertex at infinity
-#            if np.all(simplex >= 0): 
-#                
-#                # container of voronoi vertices per region
-#                pointList = []
-#                
-#                for component in simplex:
-#
-#                    pointList.append((new_vertices[component][0], new_vertices\
-#                        [component][1]))
-#
-#                if len(pointList) > 2:
-#
-#                    polygon = Polygon(pointList)
-#                    polygon = shape.intersection(polygon)
-#                    area = polygon.area
-#                    #sampleIndex = list(lookup).index(index)
-#                    corrSample = sampleList[sampleIndex]
-#                    G.add_node(sampleIndex, position = corrSample, area =\
-#                        1.0 * area / totalArea, landUse = landUse[sampleIndex]\
-#                        , pointList = pointList, polygon = polygon)    
-#        
-#    for index in G.nodes():
-#        
-#        for nindex in range(len(list(neighborList[index]))):
-#
-#            if index != neighborList[index][nindex] and neighborList[index]\
-#                [nindex] in G.nodes():
-#
-#                    G.add_edge(index, neighborList[index][nindex])
-#
-#    print "-done constructing network-"
-#    return G
 
 def construct_delaunay_networks(sample_list, land_use, regions, interior_indices):
+
     networks = []
     interior_polygons = []
+        
     for index in interior_indices:
         interior_polygons.append(Polygon(regions[index]))
     for index in [i for i in xrange(len(regions)) if i not in interior_indices]:
         region = regions[index]
         region_samples = sample_list[index]
         region_land_use = land_use[index]
-        network = construct_delaunay_network_of_region(region, region_samples, region_land_use, interior_polygons)
-        networks.append(network)
+        try:
+            network = construct_delaunay_network_of_region(region, region_samples, region_land_use, interior_polygons)
+            networks.append(network)
+        except:
+            pass
     return networks
     
 def plot_region(network, fig, ax):
     """
     Plots a region, colored by phenotype.
     """
+
+#    global countez
 
     color_dict = {
         0 : 'red',
@@ -544,6 +338,10 @@ def plot_region(network, fig, ax):
             patch = PolygonPatch(polygon, fc = color_dict[maxIndex], ec = "black",\
             aa=False)
             ax.add_patch(patch)
+            
+#            plt.savefig(str(countez).zfill(2) + ".png")
+#            countez += 1
+            
         except AssertionError:
             for p in polygon:
                 patch = PolygonPatch(p, fc = color_dict[maxIndex], ec = "black",\
@@ -552,77 +350,22 @@ def plot_region(network, fig, ax):
 
 def plot_regions(networks, dimensions, num, run, iteration, dist, scale):
     fig = plt.figure(figsize = (10, 10), facecolor = "black")
-    ax = fig.add_subplot(111)#fig.add_axes([0, 0, 1, 1])
+    ax = fig.add_axes([0, 0, 1, 1])
     ax.set_xlim(0,dimensions[0])
     ax.set_ylim(0,dimensions[1])    
     for network in networks:
         plot_region(network, fig, ax)
-    #ax.add_patch(PolygonPatch( Polygon([(0., 0.), (0., 1.), (1., 1.), (1., 0.)]), fc = 'none'))
+
     ax.set_aspect('equal')
-    plt.legend()
     plt.axis('off')
     plt.savefig('graph_sample{0}_run{1}_step{2}_{3}_std{4}.png'.\
         format(num, str(run).zfill(2), str(iteration).zfill(6), dist, scale), \
         bbox_inches='tight', pad_inches=0, facecolor=fig.get_facecolor())
     plt.close()
 
-#def plot_regions_shaded(numSample, network, shape, dist, scale, name, run, op):
-#    """
-#    Plots the map, colored by phenotype.
-#    """
-#
-#    color_dict = {
-#        0 : 'red',
-#        1 : 'green',
-#        2 : 'blue',
-#        3 : 'black',
-#        4 : 'blue',
-#        5 : 'brown'
-#    }
-#    
-#    fig = plt.figure(figsize = (10, 10))
-#    ax = fig.add_axes([0, 0, 1, 1])
-#    pointList = nx.get_node_attributes(network, 'pointList')
-#    pointList = pointList.values()
-#
-#    for index in network.nodes():
-#
-#        if list(network.node[index]['landUse']).count(max(network.node[index]\
-#            ['landUse'])) == 1:
-#
-#            maxIndex = network.node[index]['landUse'].index(max(network.node[\
-#            index]['landUse']))
-#
-#        else:
-#
-#            withMaxLandUse = []
-#
-#            for index2, value in enumerate(network.node[index]['landUse']):
-#
-#                if value == max(network.node[index]['landUse']):
-#
-#                    withMaxLandUse.append(index2)
-#
-#            maxIndex = np.random.choice(withMaxLandUse)
-#
-#        polygon = network.node[index]['polygon']
-#        patch = PolygonPatch(polygon, fc = color_dict[maxIndex], ec = "none",\
-#            aa=False)
-#        ax.add_patch(patch)
-#        
-#    #ax.add_patch(PolygonPatch( Polygon([(0., 0.), (0., 1.), (1., 1.), (1., 0.)]), fc = 'none'))
-#    ax.set_aspect('equal')
-#    ax.set_adjustable('box-forced')
-#    plt.legend()
-#    plt.axis('off')
-#    plt.savefig('map_sample{0}_{1}_{2}_scale{3}_step{4}_run{5}_op{6}.png'.\
-#        format(numSample, shape, dist, scale, str(name).zfill(6), \
-#        str(run).zfill(2), op), bbox_inches='tight', pad_inches=0)
-#    plt.close()
-            
 if __name__ == "__main__":
     
-    regionpath = "regions.png"
+    regionpath = "visayas.png"
     samplepath = "samples.png"
     dist = "uniform"
     scale = 0.2
@@ -635,4 +378,3 @@ if __name__ == "__main__":
     landUse = return_land_use(sampleList, numType)
     networks = construct_delaunay_networks(sampleList, landUse, regions, interior_indices)  
     plot_regions(networks, dimensions, numSample, run, 0, dist, scale)
-
--- extractregions.py
+++ extractregions.py
@@ -10,30 +10,43 @@ import Image
 from itertools import permutations
 from skimage.measure import find_contours
 from shapely.geometry import Polygon, Point
+import matplotlib.pyplot as plt
+from descartes.patch import PolygonPatch
+
 
 def extract_regions(filepath):
     """
     Extracts the habitable regions from filepath and returns the (x,y)
     coords in a list.
     """   
-    img = Image.open(filepath)
+    img = pad_image(filepath)
     img_as_array = np.array(img.convert('L').rotate(180)\
         .transpose(Image.FLIP_LEFT_RIGHT))
-    
+            
     # x and y are inverted
     inv_regions = find_contours(img_as_array, 0.5)
+    print len(inv_regions)
+
+    # boundary of image itself
+    map_boundary = Polygon(inv_regions[0])    
     
     # container for corrected (x, y)'s
     regions = []
     region_polygons = []
     
     # loop to reverse each (x, y)
-    for region in inv_regions:        
+    for region in inv_regions[1:]:        
         current = []        
-        for y, x in region:            
-            current.append([x, y])          
-        regions.append(current)
-        region_polygons.append(Polygon(current))
+        for y, x in region:
+            current.append((x, y))  
+        intersected = Polygon(current).intersection(map_boundary)
+        print type(intersected)
+        try:
+            regions.append(intersected.exterior.coords)
+        except AttributeError:
+            for i in intersected:
+                print type(i)
+        region_polygons.append(intersected)
     
     interior = set()
     region_index_permutations = permutations(xrange(len(regions)), 2)
@@ -44,7 +57,7 @@ def extract_regions(filepath):
     interior = list(interior)
     interior.sort()
     interior.reverse()
-            
+
     return regions, interior, img.size
     
 def extract_samples(samplepath, regions):
@@ -52,7 +65,7 @@ def extract_samples(samplepath, regions):
     Extract samples from samplepath and group them (in lists) according
     to their region of membership.
     """    
-    sample_img = Image.open(samplepath)
+    sample_img = pad_image(samplepath)
     sample_as_array = np.array(sample_img.convert('L').rotate(180)\
         .transpose(Image.FLIP_LEFT_RIGHT))
 
@@ -60,23 +73,47 @@ def extract_samples(samplepath, regions):
     for region in regions:
         region_polygons.append(Polygon(region))
     
-    samples = np.nonzero(sample_as_array)
+    samples = np.nonzero(sample_as_array[2:-2, 2:-2])
     samples = zip(samples[1], samples[0])
     
 #    samples = [(x, y) for x in xrange(int(sample_as_array.shape[0])) \
 #        for y in xrange(int(sample_as_array.shape[1])) if \
 #        sample_as_array[x, y] != 0]
 
-    sorted_samples = [[]] * len(regions)
+    print len(samples)
+    sorted_samples = dict()
+    for index in xrange(len(region_polygons)):
+        sorted_samples[index] = []
     
-    i = 0
     for sample in samples:
-        #print i
-        i+=1
         for index, region_polygon in enumerate(region_polygons):
             if region_polygon.contains(Point(sample)):
                 sorted_samples[index].append(sample)
                 break
-    
+
     return sorted_samples
     
+def pad_image(imgpath):
+    img = Image.open(imgpath)
+    img_as_array = np.array(img.convert('L'), dtype = int)
+    width, height = int(img_as_array.shape[0]), int(img_as_array.shape[1])
+    padded = np.ones((width + 4, height + 4))
+    padded.fill(255)
+    padded[1:-1, 1:-1] = 0
+    padded[2:-2, 2:-2] = img_as_array
+    img = Image.fromarray(padded).convert('RGB')
+    return img
+    
+def randomly_sample(imgpath, number):
+    img = Image.open(imgpath)
+    w, h = img.size
+    sampled = Image.new("L", (w,h))
+    
+    count = 0
+    while count < number:
+        sampled.putpixel((np.random.random_integers(0, w - 1), np.random.random_integers(0, h - 1)), 255)
+        count += 1
+    sampled.save("samples.png")
+
+if __name__ == "__main__":    
+    randomly_sample("visayas.png", 5000)
